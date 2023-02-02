e6a6c47706a471f48f22c89850a491f73776b532
Cristian Bulgarelli <cristian.bulgarelli@overit.it>
2023-02-02 11:01:53 +0100

    feature 1 - first commit - WIP

---
+++ com/example/Feature1.java
@@ -0,0 +1,19 @@
+package com.example;
+
+@SuppressWarnings("unused")
+public class Feature1 {
+
+    private String name;
+
+    public Feature1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
