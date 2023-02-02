79db365f139cdc3f24f78a93a92ef98544ec9f28
Cristian Bulgarelli <cristian.bulgarelli@overit.it>
2023-02-02 11:08:23 +0100

    feature 1 - second commit - feature complete!

--- com/example/Feature1.java
+++ com/example/Feature1.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import java.util.Objects;
+
 @SuppressWarnings("unused")
 public class Feature1 {
 
     private String name;
+    private String surname;
 
     public Feature1(String name) {
         this.name = name;
@@ -15,5 +18,34 @@ public class Feature1 {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feature1 feature1 = (Feature1) o;
+        return name.equals(feature1.name) && surname.equals(feature1.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+    @Override
+    public String toString() {
+        return "Feature1{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
     }
 }
